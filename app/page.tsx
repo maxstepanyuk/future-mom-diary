b870f5546d3b077a0798c586c1424e52964067b1
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-08-22 14:47:44 +0300

    mv contents of DashboardPageWrapper to DashboardPage at app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
-import DashboardPageWrapper from "@/components/pages/dashboard-page/DashboardPageWrapper/DashboardPageWrapper";
+import css from "./DashboardPage.module.css";
+import StatusBlock from "@/components/pages/dashboard-page/StatusBlock/StatusBlock";
+import BabyTodayCard from "@/components/pages/dashboard-page/BabyTodayCard/BabyTodayCard";
+import MomTipCard from "@/components/pages/dashboard-page/MomTipCard/MomTipCard";
+import FeelingCheckCard from "@/components/pages/dashboard-page/FeelingCheckCard/FeelingCheckCard";
 
 export default function DashboardPage() {
   return (
     <>
       <section>
-        <DashboardPageWrapper />
+        <div className={css.container}>
+          <div className={css.leftColumn}>
+            <StatusBlock />
+            <BabyTodayCard />
+            <MomTipCard />
+          </div>
+          <div className={css.rightColumn}>
+            <FeelingCheckCard />
+          </div>
+        </div>
       </section>
     </>
   );
