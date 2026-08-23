5ad96641ed58fbf6bcb8606d97fd0857a8aec8c8
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-08-23 13:30:48 +0300

    add NotFound css - final

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
   return (
     <div className={css.wrapper}>
       <h2>Сторінку не знайдено</h2>
-      <Link href="/" className={"todo"}>Певернутися на головну</Link>
+      <Link href="/" className={css.link}>Певернутися на головну</Link>
     </div>
   );
 }
