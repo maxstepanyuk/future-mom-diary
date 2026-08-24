eb5b927764c70d42a63677a48f7bd9e3306df82d
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-08-24 12:22:48 +0300

    revert ProfilePage

--- app/(private)/profile/page.tsx
+++ app/(private)/profile/page.tsx
@@ -1,18 +1,22 @@
+import { Metadata } from "next";
+
 import ProfileAvatar from "@/components/pages/profile-page/ProfileAvatar/ProfileAvatar";
 import ProfileEditForm from "@/components/pages/profile-page/ProfileEditForm/ProfileEditForm";
-import { Metadata } from "next"; 
 
-import css from "./ProfilePage.module.css"; 
+import css from "./ProfilePage.module.css";
 
 export const metadata: Metadata = {
-title: "Профіль - Лелека",
-openGraph: {
-title: "Профіль - Лелека",
-},
-}; 
+  title: "Профіль - Лелека",
+  openGraph: {
+    title: "Профіль - Лелека",
+  },
+};
 
 export default function ProfilePage() {
-return ( 
-
-);
+  return (
+    <div className={css.container}>
+      <ProfileAvatar />
+      <ProfileEditForm />
+    </div>
+  );
 }
